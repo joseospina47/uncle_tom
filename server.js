78ba3020f7b02fb82a5745aa1b6bd511ebcf2083
Jose Ospina <jospina@capmotion.net>
2018-11-23 00:09:39 -0500

    feat(Whole Project): Added invoices reminder

--- server.js
+++ server.js
@@ -1,12 +1,10 @@
 'use strict'
 
+require('./lib/app')
 const http = require('http')
-const app = require('./lib/app')
 
 http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' })
   res.write('Hi, I\'m Tom, Capmotion\'s AI Director')
   res.end()
 }).listen(process.env.PORT || 3000)
-
-app.init()
