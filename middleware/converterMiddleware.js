87082e9d9a1b406ec73127995c789947a5953138
Jose Ospina <jospina@capmotion.net>
2018-11-22 21:10:13 -0500

    fix(converterMiddleware.js): Fixed converter validations

--- middleware/converterMiddleware.js
+++ middleware/converterMiddleware.js
@@ -3,6 +3,7 @@
 const converterMiddleware = (patterns, message) => {
   const words = message.text.split(' ')
   if (words.length !== 2) return false
+  if (words[0] !== 'convert') return false
   if (isNaN(words[1])) return false
   return true
 }
